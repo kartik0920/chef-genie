ec3e1b606a0da137b82dfb28ffe8dfc2d798453c
kartik0920 <kartik0920@gmail.com>
2024-11-28 18:56:32 +0530

    Adding logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from "../assets/chef-logo.png";
+
 export default function Header() {
   return (
     <header className="title">
-      <img src="src\assets\chef-logo.png" alt="chef-logo" />
+      <img src={logo} alt="chef-logo" />
       <span>Food Genie </span>
     </header>
   );
