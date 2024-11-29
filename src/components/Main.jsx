3c2b51cfc5d0b472dd9025f1030f26492f6f9e59
kartik0920 <kartik0920@gmail.com>
2024-11-29 14:53:44 +0530

    getting recipe from dish name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Recipe from "./Recipe";
 import IngredientList from "./ingredientList";
-import { getRecipeFromMistral } from "../ai";
+import { getRecipeFromMistral, getRecipeFromName } from "../ai";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
   const [recipe, setRecipe] = React.useState("");
+  const [dish, Setdish] = React.useState(null);
 
   function addItem(event) {
     const item = event.get("list-ingredient");
     setIngredients((x) => [...x, item]);
+  }
+
+  async function addDish(e) {
+    const name = e.get("dishName");
+    Setdish(name);
+    const recipeFromAi = await getRecipeFromName(name);
+    console.log(recipeFromAi);
+    setRecipe(recipeFromAi);
   }
 
   const ingredientsListItems = ingredients.map((ingredient) => (
@@ -25,28 +34,55 @@ export default function Main() {
     <main>
       <div className="intro">
         <p>
-          Add the list of Available ingredients and Let us make the recipe for
-          you
+          Add the list of Available ingredients or Name of a Dish and Let us
+          make the recipe for you
         </p>
       </div>
-      <form className="input-form" action={addItem}>
-        <input
-          aria-label="Add ingredient"
-          type="text"
-          name="list-ingredient"
-          placeholder="e.g. oregano"
-        />
-        <button type="submit">+ Add Ingredient</button>
-      </form>
 
-      <section>
-        <IngredientList
-          list={ingredientsListItems}
-          addItem={addItem}
-          generateRecipe={getRecipe}
-        />
-        {recipe && <Recipe recipe={recipe} />}
+      {dish === null && (
+        <section className="fromIngredient">
+          <form className="input-form" action={addItem}>
+            <input
+              aria-label="Add ingredient"
+              type="text"
+              name="list-ingredient"
+              placeholder="e.g. oregano"
+            />
+            <button type="submit">+ Add Ingredient</button>
+          </form>
+
+          <section>
+            <IngredientList
+              list={ingredientsListItems}
+              addItem={addItem}
+              generateRecipe={getRecipe}
+            />
+          </section>
+        </section>
+      )}
+
+      <section className="fromDish">
+        {ingredients.length === 0 && (
+          <section>
+            {dish === null && (
+              <div className="intro">
+                <h3>OR</h3>
+              </div>
+            )}
+
+            <form className="input-form" action={addDish}>
+              <input
+                aria-label="Add dish name"
+                type="text"
+                name="dishName"
+                placeholder="e.g. Pav-bhaji"
+              />
+              <button type="submit">+ Add Dish name</button>
+            </form>
+          </section>
+        )}
       </section>
+      {recipe && <Recipe recipe={recipe} />}
     </main>
   );
 }
